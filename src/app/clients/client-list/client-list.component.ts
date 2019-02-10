a48ee9ce98277ac05e3c2d92b4942e2ab0d2d2f1
wiltrahan <twilorip@gmail.com>
2019-02-10 14:39:45 -0500

    initial commit

---
+++ src/app/clients/client-list/client-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-client-list',
+  templateUrl: './client-list.component.html',
+  styleUrls: ['./client-list.component.css'],
+  moduleId: module.id,
+})
+export class ClientListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
