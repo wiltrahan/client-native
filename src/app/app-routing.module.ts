afb541cfd07042f9ac185894b08dac119b384ad3
wiltrahan <twilorip@gmail.com>
2019-02-10 16:08:08 -0500

    begin navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from "@angular/router";
 import { AuthComponent } from "~/app/auth/auth.component";
+import { ClientListComponent } from "~/app/clients/client-list/client-list.component";
 
 const routes: Routes = [
-  { path: '', component: AuthComponent }
+  { path: '', component: AuthComponent },
+  { path: 'client-list', component: ClientListComponent }
 ];
 
 @NgModule({
