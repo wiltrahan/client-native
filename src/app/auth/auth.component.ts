ea97583d0fac078877f5b4fa873e0ff24962c339
wiltrahan <twilorip@gmail.com>
2019-02-13 17:28:33 -0500

    begin work on signin

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterExtensions } from 'nativescript-angular/router';
-
+import { User } from '../shared/user/user.model';
+import { UserService } from '~/app/shared/user/user.service';
 @Component({
   selector: 'ns-auth',
+  providers: [UserService],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
   moduleId: module.id,
 })
 export class AuthComponent implements OnInit {
-
-  constructor(private router: RouterExtensions) { }
+  // email = 'wiltrahan@us.gov';
+  user: User;
+  isLoggingIn = true;
+  
+  constructor(private router: RouterExtensions, private userService: UserService) { 
+    this.user = new User();
+  }
 
   ngOnInit() {
   }
 
-  onSignin() {
-    this.router.navigate(['/client-list'], {clearHistory: true});
+  // onSignin() {
+  //   this.router.navigate(['/client-list'], {clearHistory: true});
+  // }
+
+  submit() {
+    if(this.isLoggingIn) {
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+
+  login() {
+
+  }
+
+  signUp() {
+    this.userService.register(this.user);
+  }
+ 
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
   }
 
 }
